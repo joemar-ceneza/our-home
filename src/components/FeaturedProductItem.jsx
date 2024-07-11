c214001ca81def145e4a20888c8ed45f2d4b9843
joemar-ceneza <joemar.ceneza@gmail.com>
2024-07-11 19:22:00 +0800

    add some padding and fixing user interface

--- src/components/FeaturedProductItem.jsx
+++ src/components/FeaturedProductItem.jsx
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 export default function FeaturedProductItem({ product }) {
   return (
     <Link to={`/product/${product.attributes.slug}`}>
-      <div className="text-sm tracking-wide hover:shadow-2xl transition ease-in-out delay-75">
+      <div className="h-[380px] text-sm tracking-wide overflow-hidden hover:shadow-2xl transition ease-in-out delay-75">
         <img src={`${product.attributes.image.data.attributes.url}`} alt="" />
-        <h3 className="font-bold">{product.attributes.title}</h3>
-        <p>₱ {product.attributes.regPrice.toLocaleString()}</p>
+        <div className="py-3 px-2">
+          <h3 className="font-bold">{product.attributes.title}</h3>
+          <p className="my-2">
+            ₱ {product.attributes.regPrice.toLocaleString()}
+          </p>
+        </div>
       </div>
     </Link>
   );
