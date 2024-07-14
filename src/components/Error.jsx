24e9fe5c54588090a030f1df2e3f83c80e3cdfee
joemar-ceneza <joemar.ceneza@gmail.com>
2024-07-15 06:44:57 +0800

    add new spinnerloader and error file

---
+++ src/components/Error.jsx
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <div className="w-full h-[150px] flex justify-center items-center text-red-500 text-center font-bold ">
+      Error Loading Products...
+    </div>
+  );
+}
