04395b14a405d20aac685b0264fb24e4680fc948
joemar-ceneza <joemar.ceneza@gmail.com>
2024-07-20 23:25:16 +0800

    make spinner max full width

--- src/components/SpinnerLoader.jsx
+++ src/components/SpinnerLoader.jsx
@@ -1,6 +1,6 @@
 export default function SpinnerLoader() {
   return (
-    <div className="w-full h-[150px] mx-auto flex justify-center items-center transform">
+    <div className="max-w-full h-[150px] mx-auto flex justify-center items-center transform">
       <div className="border-t-transparent border-solid animate-spin rounded-full border-blue-400 border-8 h-14 w-14"></div>
     </div>
   );
