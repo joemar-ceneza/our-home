968b26f922681dedae297e19c46e71680dfcfab2
joemar-ceneza <joemar.ceneza@gmail.com>
2024-07-21 01:14:19 +0800

    added notfound components

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="w-full h-[150px] flex justify-center items-center text-red-500 text-center font-bold ">
+      Product not found
+    </div>
+  );
+}
