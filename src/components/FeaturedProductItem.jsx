98781b7ffe8a1517d5baad60d6c099a61fd0a030
joemar-ceneza <joemar.ceneza@gmail.com>
2024-07-31 15:54:34 +0800

    fixing padding

--- src/components/FeaturedProductItem.jsx
+++ src/components/FeaturedProductItem.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function FeaturedProductItem({ product }) {
   return (
     <Link to={`/product/${product.attributes.slug}`}>
-      <div className="h-[380px] text-sm tracking-wide overflow-hidden hover:shadow-2xl transition ease-in-out delay-75">
+      <div className="h-[380px] text-sm tracking-wide overflow-hidden hover:shadow-2xl transition ease-in-out delay-75 rounded-3xl">
         <img src={`${product.attributes.image.data.attributes.url}`} alt="" />
         <div className="py-3 px-2">
           <h3 className="font-bold">{product.attributes.title}</h3>
