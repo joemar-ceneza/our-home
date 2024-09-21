2b019e05dad6fbfffa15ad4694fb899c134021b8
joemar-ceneza <joemar.ceneza@gmail.com>
2024-09-21 23:47:50 +0800

    fixing endpoint url for the new backend

--- src/components/NewProducts.jsx
+++ src/components/NewProducts.jsx
@@ -4,7 +4,7 @@ export default function NewProducts() {
   return (
     <ProductSection
       title="new products"
-      fetchUrl="/products?populate=*&filters[isNew]=true"
+      fetchUrl="/products/products?isNewProduct=true"
       bestLabel={false}
       newLabel={true}
       relatedLabel={false}
