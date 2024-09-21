02ae7578040318d5d994f227c16e19a42d0bccdb
joemar-ceneza <joemar.ceneza@gmail.com>
2024-09-21 23:48:20 +0800

    fixing the data for the new backend

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -25,14 +25,10 @@ export default function Product({
   };
 
   return (
-    <Link to={`/product/${product.attributes.slug}`} draggable="false">
+    <Link to={`/product/${product.slug}`} draggable="false">
       <div className="bg-slate-200 w-[230px] h-[320px] mx-auto rounded-[8px] relative group">
         <div className="w-full h-[200px] flex flex-col items-center justify-center">
-          <img
-            className="w-4/5 h-[160px]"
-            src={`${product.attributes.image.data.attributes.url}`}
-            alt=""
-          />
+          <img className="w-4/5 h-[160px]" src={`${product.image}`} alt="" />
           <div className="absolute bottom-[140px] w-4/5 flex justify-around text-sm opacity-0 text-white group-hover:opacity-100 group-hover:bg-gray-700 transition ease-in-out delay-75">
             <button
               className="w-full flex justify-around items-center uppercase p-1 hover:bg-gray-500"
@@ -49,42 +45,47 @@ export default function Product({
         </div>
         <div className="text-gray-700 text-sm tracking-wide mx-8">
           <div className=" font-bold capitalize tracking-wider mb-1">
-            {product.attributes.categories.data[0].attributes.title}
+            {product.categories}
           </div>
           <div className="capitalize tracking-wide">
-            {product.attributes.title.substring(0, 18)}...
+            {product.name.substring(0, 18)}...
           </div>
-          {bestLabel &&
-            product.attributes.isOnSale &&
-            product.attributes.salePrice !== 0 && (
-              <div className="absolute bg-red-500 text-white text-xs font-bold uppercase top-8 left-8 px-2 py-1 z-10">
-                sale
-              </div>
-            )}
-          {newLabel && product.attributes.isNew && (
+          {bestLabel && product.isOnSale && product.salePrice !== 0 && (
+            <div className="absolute bg-red-500 text-white text-xs font-bold uppercase top-8 left-8 px-2 py-1 z-10">
+              sale
+            </div>
+          )}
+          {newLabel && product.isNewProduct && (
             <div className="absolute bg-green-500 text-white text-xs font-bold uppercase top-8 left-8 px-2 py-1 z-10">
               new
             </div>
           )}
-          {relatedLabel &&
-            product.attributes.isOnSale &&
-            product.attributes.salePrice !== 0 && (
-              <div className="absolute bg-red-500 text-white text-xs font-bold uppercase top-8 left-8 px-2 py-1 z-10">
-                sale
-              </div>
-            )}
-          {product.attributes.isOnSale && product.attributes.salePrice !== 0 ? (
+          {relatedLabel && product.isOnSale && product.salePrice !== 0 && (
+            <div className="absolute bg-red-500 text-white text-xs font-bold uppercase top-8 left-8 px-2 py-1 z-10">
+              sale
+            </div>
+          )}
+          {product.isOnSale && product.salePrice !== 0 ? (
             <>
               <div className="absolute bottom-8">
-                ₱{product.attributes.salePrice.toLocaleString()}
+                ₱{" "}
+                {product.salePrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               </div>
               <div className="absolute bottom-8 left-28 text-gray-400 line-through">
-                ₱{product.attributes.regPrice.toLocaleString()}
+                ₱{" "}
+                {product.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
               </div>
             </>
           ) : (
             <div className="absolute bottom-8">
-              ₱{product.attributes.regPrice.toLocaleString()}
+              ₱{" "}
+              {product.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
             </div>
           )}
         </div>
