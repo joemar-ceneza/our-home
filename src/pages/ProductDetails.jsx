4a600eca5ffffa9cae63ab5a1099d8adfed4772e
joemar-ceneza <joemar.ceneza@gmail.com>
2024-09-22 00:00:22 +0800

    fixing the data for the new backend

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -41,7 +41,7 @@ export default function ProductsDetails() {
   }
 
   const product = data[0];
-  const categoryTitle = data[0].attributes.categories.data[0].attributes.title;
+  const categoryTitle = data[0].name;
 
   return (
     <section className="max-w-screen-2xl w-[90%] mx-auto py-10">
@@ -50,32 +50,21 @@ export default function ProductsDetails() {
           <IoHomeOutline />
         </Link>
         <IoChevronForward className="mx-2" />
-        <Link
-          to={`/products/${data[0].attributes.categories.data[0].attributes.slug}`}>
-          <p className="text-sm tracking-wide capitalize">
-            {data[0].attributes.categories.data[0].attributes.title}
-          </p>
+        <Link to={`/products/${data[0].slug}`}>
+          <p className="text-sm tracking-wide capitalize">{data[0].name}</p>
         </Link>
         <IoChevronForward className="mx-2" />
-        <p className="text-sm tracking-wide capitalize">
-          {data[0].attributes.title}
-        </p>
+        <p className="text-sm tracking-wide capitalize">{data[0].title}</p>
       </div>
       <div className="flex justify-between max-lg:flex-col">
-        <img
-          className="w-1/2 max-lg:w-full"
-          src={data[0].attributes.image?.data.attributes.url}
-          alt=""
-        />
+        <img className="w-1/2 max-lg:w-full" src={data[0].image} alt="" />
         <div className="w-1/2 p-10 max-lg:w-full max-lg:px-0">
           <h2 className="text-3xl font-medium text-gray-700">
-            {data[0].attributes.title}
+            {data[0].title}
           </h2>
-          <p className="py-3">
-            ₱ {data[0].attributes.regPrice.toLocaleString()}
-          </p>
+          <p className="py-3">₱ {data[0].regularPrice.toLocaleString()}</p>
           <hr />
-          <p className="py-5 tracking-wide">{data[0].attributes.description}</p>
+          <p className="py-5 tracking-wide">{data[0].description}</p>
           <button
             onClick={() => addToCart(product)}
             className="bg-gray-800 my-7 p-3 px-16 text-white capitalize max-lg:w-full">
