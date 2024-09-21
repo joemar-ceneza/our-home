a8111168ccb5160a2ff5e45caa47eb29972c598d
joemar-ceneza <joemar.ceneza@gmail.com>
2024-09-21 23:46:57 +0800

    fixing the data for the new backend

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 export default function CategoryItem({ category }) {
   return (
     <div className="relative w-full">
-      <Link to={`/products/${category.attributes.slug}`}>
+      <Link to={`/products/${category.slug}`}>
         <img
           className="w-full brightness-50"
           key={category.id}
-          src={`${category.attributes.image.data.attributes.url}`}
-          alt={category.attributes.title}
+          src={`${category.image}`}
+          alt={category.name}
           draggable="false"
         />
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 uppercase underline underline-offset-[10px] font-bold text-white text-xs w-full text-center sm:text-sm">
-          {category.attributes.title}
+          {category.name}
         </div>
       </Link>
     </div>
