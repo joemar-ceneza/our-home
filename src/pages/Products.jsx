f8d5bf4b95a1f209cd7f7008d287cda8fdb50d42
joemar-ceneza <joemar.ceneza@gmail.com>
2024-09-25 03:15:44 +0800

    changed the endpoint of the URL

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -11,16 +11,14 @@ import {
 
 export default function Products() {
   const { id } = useParams();
-  const { data } = useFetch(
-    `/products?populate=*&filters[categories][slug][$eq]=${id}`
-  );
+  const { data } = useFetch(`/categories/categories/${id}`);
   const [title, setTitle] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
 
   useEffect(() => {
     if (data) {
-      setTitle(data[0].name);
+      setTitle(data?.category.name);
     }
   }, [data]);
 
@@ -28,7 +26,7 @@ export default function Products() {
     <section className="max-w-screen-xl w-[90%] mx-auto py-10">
       <h2 className="py-5 uppercase text-xl text-center font-bold">{title}</h2>
       <div className="max-w-[230px] mx-auto py-10 md:max-w-[500px] lg:max-w-[770px] xl:max-w-[1040px] grid grid-cols-1 gap-[15px] md:grid-cols-2 md:gap-[40px] lg:grid-cols-3 xl:grid-cols-4">
-        {data?.map((product) => {
+        {data?.products.map((product) => {
           return (
             <Product
               product={product}
