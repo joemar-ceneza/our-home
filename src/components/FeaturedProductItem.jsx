dfc3a5d7c26649ccb3f637ab57c871014a026208
joemar-ceneza <joemar.ceneza@gmail.com>
2024-09-25 05:00:48 +0800

    fixing endpoint of the URL and adjusting data for the new backend

--- src/components/FeaturedProductItem.jsx
+++ src/components/FeaturedProductItem.jsx
@@ -2,14 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function FeaturedProductItem({ product }) {
   return (
-    <Link to={`/product/${product.attributes.slug}`}>
+    <Link to={`/product/${product.slug}`}>
       <div className="h-[380px] text-sm tracking-wide overflow-hidden hover:shadow-2xl transition ease-in-out delay-75 rounded-3xl">
-        <img src={`${product.attributes.image.data.attributes.url}`} alt="" />
+        <img src={`${product.image}`} alt="" />
         <div className="py-3 px-2">
-          <h3 className="font-bold">{product.attributes.title}</h3>
-          <p className="my-2">
-            ₱ {product.attributes.regPrice.toLocaleString()}
-          </p>
+          <h3 className="font-bold">{product.name}</h3>
+          <p className="my-2">₱ {product.regularPrice.toLocaleString()}</p>
         </div>
       </div>
     </Link>
