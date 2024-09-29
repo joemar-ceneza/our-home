eb03bbb21d6cd34f14d752d2d9d9a98904a2b458
joemar-ceneza <joemar.ceneza@gmail.com>
2024-09-29 16:28:19 +0800

    import images large screen and small screen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,19 @@ import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import useFetch from "../hook/useFetch";
+// images large screen sizes
+import DesktopSale from "../images/desktop-sale.jpg";
+import DesktopInteriorDesign from "../images/desktop-interior-design.jpg";
+import DesktopInstallment from "../images/desktop-installment.jpg";
+import DesktopShoppingReward from "../images/desktop-shopping-reward.jpg";
+import DesktopLess from "../images/desktop-less.jpg";
+// images small screen sizes
+import MobileExtra from "../images/mobile-extra.jpeg";
+import MobileGiftsDad from "../images/mobile-gifts-dad.jpeg";
+import MobileInteriorDesign from "../images/mobile-interior-design.jpeg";
+import MobileLess from "../images/mobile-less.jpeg";
+import MobileMidYearSale from "../images/mobile-mid-year-sale.jpeg";
+import MobileInstallment from "../images/mobile-installment.jpeg";
 
 export default function Hero() {
   const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768); // Initial check for screen size
