3ceb4c26ba8f8287cff4930e8aaa61de18b63886
joemar-ceneza <joemar.ceneza@gmail.com>
2024-10-01 03:11:23 +0800

    customize notfound component with tailwind css styling for 404 errors

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,7 +1,14 @@
 export default function NotFound() {
   return (
-    <div className="w-full h-[150px] flex justify-center items-center text-red-500 text-center font-bold ">
-      Product not Found...
+    <div className="flex flex-col items-center justify-center text-center bg-gray-100 py-60">
+      <h1 className="text-6xl font-bold text-red-500">404</h1>
+      <h2 className="text-2xl mt-4">Page Not Found</h2>
+      <p className="mt-2 text-gray-700">
+        Sorry, the page you are looking for does not exist.
+      </p>
+      <a href="/" className="mt-4 text-blue-500 hover:underline">
+        Go back to Homepage
+      </a>
     </div>
   );
 }
