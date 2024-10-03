e67e64dded51f39175fa05a769fe032c612dbad9
joemar-ceneza <joemar.ceneza@gmail.com>
2024-10-03 18:56:46 +0800

    enhanced user interface for success components

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,3 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Success() {
-  return <div>Success</div>;
+  return (
+    <div className="flex flex-col items-center justify-center py-60">
+      <h1 className="text-4xl font-bold mb-4 text-green-500">
+        Payment Successful!
+      </h1>
+      <p className="text-lg mb-4">Thank you for your purchase.</p>
+      <Link to={"/"} className=" text-blue-500 hover:underline">
+        Go back to Homepage
+      </Link>
+    </div>
+  );
 }
