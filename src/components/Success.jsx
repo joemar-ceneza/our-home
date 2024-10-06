83d7a95aaa36364a4ab0cb1855339c7355f16483
joemar-ceneza <joemar.ceneza@gmail.com>
2024-10-06 09:26:39 +0800

    call the clearCart function when the components mounts

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { CartContext } from "../context/CartContext";
 
 export default function Success() {
+  const { clearCart } = useContext(CartContext);
+  useEffect(() => {
+    clearCart();
+  }, [clearCart]);
   return (
     <div className="flex flex-col items-center justify-center py-60">
       <h1 className="text-4xl font-bold mb-4 text-green-500">
