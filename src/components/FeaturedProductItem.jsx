b3ead7cc633f8cad92f7f3bbf7762956bfe1b66d
joemar-ceneza <joemar.ceneza@gmail.com>
2024-10-08 08:31:19 +0800

    adjust some code for better user interface and fixing the price of the product

--- src/components/FeaturedProductItem.jsx
+++ src/components/FeaturedProductItem.jsx
@@ -6,8 +6,19 @@ export default function FeaturedProductItem({ product }) {
       <div className="h-[380px] text-sm tracking-wide overflow-hidden hover:shadow-2xl transition ease-in-out delay-75 rounded-3xl">
         <img src={`${product.image}`} alt="" />
         <div className="py-3 px-2">
-          <h3 className="font-bold">{product.name}</h3>
-          <p className="my-2">₱ {product.regularPrice.toLocaleString()}</p>
+          <h3 className="font-bold">{product.name.substring(0, 18)}...</h3>
+          <div className="flex">
+            {product.isOnSale && product.salePrice > 0 ? (
+              <>
+                <p className="my-2">₱ {product.salePrice.toLocaleString()}</p>
+                <p className="text-gray-300 line-through my-2 px-5">
+                  ₱ {product.regularPrice.toLocaleString()}
+                </p>
+              </>
+            ) : (
+              <p className="my-2">₱ {product.regularPrice.toLocaleString()}</p>
+            )}
+          </div>
         </div>
       </div>
     </Link>
