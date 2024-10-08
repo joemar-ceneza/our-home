604c5e7cf238a661df198af813df97b74c1cff38
joemar-ceneza <joemar.ceneza@gmail.com>
2024-10-09 07:26:54 +0800

    Enhance loading and error handling: add return statement for loading, error and not found states

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,9 +3,12 @@ import useFetch from "../hook/useFetch";
 import MobileChooseCategory from "../images/mobile-choose-category.jpg";
 import DesktopChooseCategory from "../images/desktop-choose-category.jpg";
 import CategoryItem from "./CategoryItem";
+import SpinnerLoader from "./SpinnerLoader";
+import Error from "./Error";
+import NotFound from "./NotFound";
 
 export default function Category() {
-  const { data } = useFetch("/categories");
+  const { data, isLoading, error } = useFetch("/categories");
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768); // Initial check for mobile screen size
 
   useEffect(() => {
@@ -16,6 +19,30 @@ export default function Category() {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  if (isLoading) {
+    return (
+      <div className="py-32">
+        <SpinnerLoader />
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="py-32">
+        <Error />
+      </div>
+    );
+  }
+
+  if (!data || data.length === 0) {
+    return (
+      <div className="py-32">
+        <NotFound />
+      </div>
+    );
+  }
 
   return (
     <section className="w-[95%] mx-auto py-5 select-none">
